2b31c7d581df0a8aa35223f03edb0fb8e60506e5
Harry <hjwp2@cantab.net>
2015-01-08 21:00:24 +0000

    get all files from a tree

--- dirdiff.py
+++ dirdiff.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+import os
+
+
+def get_all_files(target_dir):
+    all_files = set()
+    for root, _, files in os.walk(target_dir):
+        all_files.update(
+            [os.path.join(root, f) for f in files]
+        )
+    return all_files
 
 
 def compare(source, dest):
--- test_dirdiff.py
+++ test_dirdiff.py
@@ -1,4 +1,24 @@
-from dirdiff import compare
+from dirdiff import (
+    compare,
+    get_all_files,
+)
+import os
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def test_get_all_files_in_tree():
+    test_path = os.path.join(THIS_DIR, 'test_data', '2')
+    relative_paths = [
+         'dir1/dir2/file1',
+         'file3',
+         'dir1/file2',
+         'dir1/file0',
+    ]
+    expected_paths = set(
+        os.path.join(THIS_DIR, 'test_data', '2', f)
+        for f in relative_paths
+    )
+    assert get_all_files(test_path) == expected_paths
 
 
 def test_compare_smoke():
